4406b5379125107075000e820746650ff5021400
BY <byagihas@gmail.com>
2020-01-14 22:04:21 -0600

    Initial push

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const fs = require('fs')
+const request = require('request')
+const rp = require('request-promise')
+
+class App extends Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      ingredients: null,
+      petfood: null,
+      petid: null
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  componentDidMount() {
+    
+  }
+
+  handleChange(event) {
+    //fetch(`https://www.chewy.com/s?query=${this.state.petfood}`)
+      //.then(response => response.json())
+      //.then(data => this.setState({ ingredients: data }));
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.setState({petfood: this.element.value});
+    fetch(`https://www.chewy.com/s?query=${this.element.value}`)
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        this.setState({ingredients: data })
+        return(
+          <div>
+            {this.state.ingredients}
+          </div>
+        )
+      });
+    
+    
+  }
+
+  render() {
+      return (
+        <div>
+          <form onSubmit={this.handleSubmit}>
+            <input type='text' name='element' ref={el => this.element = el} />
+            <input type='submit' value='Submit' />
+          </form>
+          <span>{this.state.petfood}</span>
+          <span>{this.state.ingredients}</span>
+        </div>
+      );
+  }
 }
 
 export default App;
